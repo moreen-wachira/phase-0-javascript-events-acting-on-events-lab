03aa86bca4eaa6b91a32c673b9c1ce2326fee957
Moreen <moreencharles@gmail.com>
2023-10-29 18:37:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) { // Assuming the game field has a width of 400px and the dodger is 40px wide
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  const dodger = document.getElementById("dodger");
